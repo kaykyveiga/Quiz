61be7cf31e916317d9b79173dd1656c942cf1cce
kaykyveiga <kaykyveiga46@gmail.com>
2023-09-07 14:29:05 -0300

    question creation framework

---
+++ src/data/allQuestion.js
@@ -0,0 +1,429 @@
+const data = [
+    {
+        category: "naturalSciences",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "worldHistory",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "geography",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "popCulture",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "sports",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "literature",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "technologyInovation",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+    {
+        category: "generalCuriosities",
+        questions: [
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+            {
+                question: "",
+                options: ["", "", "", ""],
+                answer: "",
+                tip: "",
+            },
+        ],
+    },
+
+]
+
+export default data
